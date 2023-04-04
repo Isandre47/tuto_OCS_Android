b80032bcd6bc297f5dd0667d2bb39b268cc1dbc6
Isandre47 <nuanda@live.fr>
2023-04-04 11:45:07 +0200

    Init and end of day, create mmodel

---
+++ app/src/main/java/com/example/myapplication/model/QuestionBank.java
@@ -0,0 +1,18 @@
+package com.example.myapplication.model;
+
+import java.util.List;
+
+public class QuestionBank {
+
+    private List<Question> mQuestionList;
+    private int mNextQuestionIndex;
+
+    public QuestionBank(List<Question> questionList) {
+        // Shuffle the question list before storing it
+    }
+
+//    public Question getNextQuestion() {
+        // Loop over the questions and return a new one at each call
+//    }
+}
+
